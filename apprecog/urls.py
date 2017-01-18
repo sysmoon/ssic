510f8d7f87470e17919f1e5b4f21ff4fcfba177b
sysmoon <sysmoon@gmail.com>
2017-01-18 11:16:18 +0900

    change the html for the site

---
+++ apprecog/urls.py
@@ -0,0 +1,7 @@
+__author__ = 'sysmoon'
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
--- apprecog/views.py
+++ apprecog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, "apprecog/post_list.html", {})
